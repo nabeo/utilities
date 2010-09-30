6445ffbda8e2556c12c0a6ba9357ec6813d07e3b
nabeo <watanabe.michikazu@gmail.com>
2010-09-30 19:17:00 +0900

    add mk_photo_catalog.rb

---
+++ ruby/mk_photo_catalog.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby192
+#encoding:utf-8
+
+require "fileutils"
+
+require "rubygems"
+require "exifr"
+
+DIST_PREFIX = "/data00/Picts/Photos"
+TARGET_DIR = ARGV.shift
+
+def copy_file(orig_file, dist_path)
+  orig_file_name = File.basename(orig_file)                        
+  print("#{orig_file_name} ... ")
+  orig_file_ext = File.extname(orig_file_name)
+  orig_file_no_ext = File.basename(orig_file_name, orig_file_ext)
+
+  # コピー先のディレクトリを作成
+  FileUtils.mkdir_p(dist_path)
+  
+  if File.exist?(dist_path + orig_file_name) == false
+    # 何も考えずにコピーできる
+    FileUtils.copy_file(orig_file, dist_path + "/" + orig_file_name)
+    print("copy.\n")
+  else
+    # 同名のファイルがコピー先にある
+    i = Dir.glob(dist_path + orig_file_no_ext + "*").length.to_s
+    dist_path_file_name = orig_file_no_ext + "-" + i + orig_file_ext
+    copy_flag = false
+    # 既に同じファイルがあればコピーしない
+    Dir.glob(dist_path + orig_file_no_ext + "*").each do |dist_exist_file|
+      if FileUtils.cmp(orig_file, dist_exist_file)
+        copy_flag = true
+      end
+    end
+    # 同名で異なるファイルがあればコピーする
+    if copy_flag == false
+      FileUtils.copy_file(orig_file, dist_path + "/" + dist_path_file_name, true)
+      print("copy.\n")
+    else
+      print("nothing to do.\n")
+    end
+  end
+end
+
+# jpegファイルの処理
+Dir.glob(TARGET_DIR + "/**/*.{jpg,JPG}") do |jpg_file|
+  jpg = EXIFR::JPEG.new(jpg_file)
+  t = jpg.exif.date_time_original
+  shoot_time = {:year => t.strftime("%Y"), :month => t.strftime("%m"), :day => t.strftime("%d")}
+  dist_jpg_dir = DIST_PREFIX + "/" + shoot_time[:year] + "/" + shoot_time[:month] + "/" + shoot_time[:day] + "/"
+  copy_file(jpg_file, dist_jpg_dir)
+end
+
+# rawファイル
+Dir.glob(TARGET_DIR + "/**/*.{nef,NEF}") do |raw_file|
+  raw = EXIFR::TIFF.new(raw_file)
+  t = raw.date_time_original
+  shoot_time = {:year => t.strftime("%Y"), :month => t.strftime("%m"), :day => t.strftime("%d")}
+  dist_raw_dir = DIST_PREFIX + "/raw/" + shoot_time[:year] + "/" + shoot_time[:month] + "/" + shoot_time[:day] + "/"
+  copy_file(raw_file, dist_raw_dir)
+end
