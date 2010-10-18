f925f79571b15619ef86a8b51c45a3a3b911cdc6
nabeo <watanabe.michikazu@gmail.com>
2010-10-18 19:46:45 +0900

    modify

--- ruby/itunes_music_library_exporter.rb
+++ ruby/itunes_music_library_exporter.rb
@@ -17,8 +17,10 @@ tracks_hash.each do |track_hash|
   track_hash.shift
   
   track_hash.each do |track_info|
+    printf(track_info["Artist"] + "'s " + track_info["Name"] + " in " + track_info["Album"] + " at " + track_info["Track Number"].to_s + "\n")
     mp3_file_name = sprintf("%02d", track_info["Track Number"]) + " " + track_info["Name"] + ".mp3"
     mp3_file = Dir.glob("mp3/**/" + mp3_file_name).shift
+
     renamed_mp3_file_name = "track-" + sprintf("%02d", track_info["Track Number"]) + ".mp3"
     if track_info["Compilation"] == true
       renamed_mp3_file_dir = "mp3_rename/va/" + track_info["Album"]
@@ -27,6 +29,7 @@ tracks_hash.each do |track_hash|
     end
     FileUtils.mkdir_p(renamed_mp3_file_dir)
     renamed_mp3_file = renamed_mp3_file_dir + "/" + renamed_mp3_file_name
+    printf("copy to : " + renamed_mp3_file + "\n")
     FileUtils.cp(mp3_file, renamed_mp3_file)
     tag = ID3Lib::Tag.new(renamed_mp3_file)
     tag.title = track_info["Name"]
@@ -34,5 +37,7 @@ tracks_hash.each do |track_hash|
     tag.performer = track_info["Artist"]
     tag.track = track_info["Track Number"].to_s + "/" + track_info["Track Count"].to_s
     tag.update!
+    printf("id3v2 tag update.\n")
+    printf("\n")
   end
 end
